bb1107472c509ba373d11de9107a1ef9bed1b9db
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-26 00:24:22 -0400

    Solved lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+function logDriverNames(array) {
+  array.forEach(function (el) {
+    console.log(el.name);
+  });
+};
+
+function logDriversByHometown(array, location) {
+  array.forEach(function (el) {
+    if (el.hometown === location) {
+    console.log(el.name);
+    }
+  });
+};
+
+function driversByRevenue(array) {
+  const newArray = [...array];
+  return newArray.sort(function (el1, el2) {
+    return el1.revenue - el2.revenue;
+  } );
+}
+
+
+function driversByName(array) {
+  const newArray = [...array];
+  return newArray.sort(function (el1, el2) {
+    return el1.name.localeCompare(el2.name);;
+  } );
+}
+
+const addRevenue = function (agg, el) {
+  return agg + el.revenue;
+}
+
+function totalRevenue(array) {
+  return array.reduce(addRevenue, 0);
+}
+
+function averageRevenue(array) {
+  return array.reduce(addRevenue, 0)/array.length;
+}
